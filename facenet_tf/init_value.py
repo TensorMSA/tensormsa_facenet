9fe44354e06cf0bfa46bc4d798d0427f41a8ad66
parksungchan <soul8085@gmail.com>
2018-01-10 06:38:56 +0000

    update : pre model : down

--- facenet_tf/init_value.py
+++ facenet_tf/init_value.py
@@ -1,4 +1,6 @@
 import os
+import facenet_tf.src.common.download_and_extract as download_and_extract
+
 
 class init_value():
     def init(self):
@@ -20,7 +22,14 @@ class init_value():
 
         # Classifier
         self.model_dir = self.project_dir + 'pre_model/'
-        self.model = self.model_dir + '20170512-110547/20170512-110547.pb'
+        if not os.path.exists(self.model_dir):
+            os.makedirs(self.model_dir)
+
+        # get pre Model Down
+        pre_model_name = '20170512-110547'
+        self.model = self.model_dir + pre_model_name+'/'+pre_model_name+'.pb'
+        download_and_extract.download_and_extract_file(pre_model_name, self.model_dir)
+
         self.classifier_filename = self.model_dir + 'my_classifier_detect.pkl'
         self.use_split_dataset = False
         self.data_dir = self.output_dir
@@ -39,7 +48,44 @@ class init_value():
         self.viewImageSizeX =3
         self.viewImageSizeY =3
 
+        # feature train
+        self.model_def = 'models.inception_resnet_v1'
+        self.logs_base_dir=self.model_dir
+        self.models_base_dir=self.model_dir
+        self.gpu_memory_fraction=1.0
+        self.data_dir= self.output_dir    # '~/datasets/casia/casia_maxpy_mtcnnalign_182_160'
+        self.max_nrof_epochs=500
+        self.batch_size=90
+        self.epoch_size=1000
+        self.embedding_size=128
+        self.keep_probability=1.0
+        self.weight_decay=0.0
+        self.center_loss_factor=0.0
+        self.center_loss_alfa=0.95
+        self.optimizer='ADAGRAD'
+        self.learning_rate=0.1
+        self.learning_rate_decay_epochs=100
+        self.learning_rate_decay_factor=1.0
+        self.moving_average_decay=0.9999
+        self.seed=666
+        self.nrof_preprocess_threads=4
+        self.learning_rate_schedule_file='data/learning_rate_schedule.txt'
+        self.filter_filename=''
+        self.filter_percentile=100.0
+        self.filter_min_nrof_images_per_class=0
+        self.pretrained_model= None # self.model
 
+        # Parameters for validation on LFW
+        self.lfw_pairs='data/pairs.txt'
+        self.lfw_file_ext='png'
+        self.lfw_dir=''
+        self.lfw_batch_size=100
+        self.lfw_nrof_folds=10
+
+        self.random_flip = True
+        self.random_rotate = True
+        self.random_crop = True
+        self.log_histograms = False
 
 
 
--- facenet_tf/src/classifier_run.py
+++ facenet_tf/src/classifier_run.py
@@ -3,26 +3,15 @@ from facenet_tf.src.align.align_dataset_mtcnn import main as align_main
 from facenet_tf.src.common.classifier import main as classifier_main
 import facenet_tf.src.common.utils as utils
 
-class align_init():
-    def __init__(self):
-        init_value.init_value.init(self)
-
 class Facenet_run():
     def run(self):
-        self = align_init()
+        init_value.init_value.init(self)
 
-        # object detect
-        align_main(self)
-
-        # get pre Model Path
-
-        pre_model_url = 'https://drive.google.com/uc?id=0B5MzpY9kBtDVZ2RpVDYwWmxoSUk&export=download'
-        pre_model_zip = self.project_dir + 'pre_model/20170512-110547.zip'
-
-        utils.get_pre_model_path(pre_model_url, pre_model_zip, self.model_dir)
-
-        # # classifier Train
-        classifier_main(self)
+        # # object detect
+        # align_main(self)
+        #
+        # # # classifier Train
+        # classifier_main(self)
 
 if __name__ == '__main__':
     Facenet_run().run()
